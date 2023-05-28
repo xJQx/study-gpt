0b3ef5e2c11b545d7b66edf44f2f2c512a33785f
Jing Qiang <tohjingqiang@gmail.com>
2023-05-28 17:52:25 +0800

    chore: set up eslint, prettier, and remove boilerplate code

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowParens: "avoid",
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  semi: true,
+  tabWidth: 2,
+  trailingComma: "none",
+};
+
+module.exports = options;
