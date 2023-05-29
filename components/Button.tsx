4b55d4e6ca4d4dd3a5223c9d4825b58e0949af98
Jing Qiang <tohjingqiang@gmail.com>
2023-05-29 21:32:33 +0800

    feat: add user modal

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,22 @@
+import {
+  Button as ChakraButton,
+  ButtonProps as ChakraButtonProps
+} from '@chakra-ui/react';
 import React from 'react';
 
-export const Button = () => {
-  return <div>Button</div>;
+interface ButtonPrps extends ChakraButtonProps {
+  children?: React.ReactNode;
+}
+
+export const Button = (props: ButtonPrps) => {
+  const { children, ...buttonProps } = props;
+
+  return (
+    <ChakraButton
+      {...buttonProps}
+      className="!bg-brand-pink hover:!bg-brand-red text-white !rounded"
+    >
+      {children}
+    </ChakraButton>
+  );
 };
