1aed720dd6668f455c9f9a6eb329f4120b5dd892
yantattan <84306837+yantattan@users.noreply.github.com>
2023-05-29 22:28:09 +0800

    chore: reformatting to prettier expectation

--- common/config/FirebaseService.ts
+++ common/config/FirebaseService.ts
@@ -1,15 +1,15 @@
 import { initializeApp } from 'firebase/app';
-import {getAuth} from "firebase/auth"
-import {getDatabase} from "firebase/database"
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    databaseURL: process.env.DATABASE_URL
-}
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  databaseURL: process.env.DATABASE_URL
+};
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
