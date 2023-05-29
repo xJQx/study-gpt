6e9cbb67fe0c3bfc086172b482abe0351845c68d
Setsuna Yukiii <siang-meng@live.com>
2023-05-29 22:07:59 +0800

    chore: linting

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 // import {loginWithGoogle} from '@/features/authentication/auth';
-import { signInWithPopup, GoogleAuthProvider} from "firebase/auth";
-import { auth } from "../common/config/FirebaseService"
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { auth } from '../common/config/FirebaseService';
 
 function login() {
-    const handleLogin = async () => {
-        const googleAuth = new GoogleAuthProvider();
-        await signInWithPopup(auth, googleAuth);  
-    }
-    return (
-        <div>
-            <button onClick = {()=>{handleLogin()}}>Login</button>
-            
-        </div>
-    );
+  const handleLogin = async () => {
+    const googleAuth = new GoogleAuthProvider();
+    await signInWithPopup(auth, googleAuth);
+  };
+  return (
+    <div>
+      <button
+        onClick={() => {
+          handleLogin();
+        }}
+      >
+        Login
+      </button>
+    </div>
+  );
 }
 
 export default login;
