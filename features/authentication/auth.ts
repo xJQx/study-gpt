0f0c2299bb804adf2442f12bdf23b588e68565b8
Setsuna Yukiii <siang-meng@live.com>
2023-05-29 16:45:50 +0800

    feat: authentication (WIP)
    the login.tsx may be deleted later

---
+++ features/authentication/auth.ts
@@ -0,0 +1,9 @@
+import { signInWithPopup, GoogleAuthProvider} from "firebase/auth";
+import { auth } from "@/common/config/FirebaseService";
+const loginWithGoogle = () => 
+{
+    const googleAuth = new GoogleAuthProvider();
+    signInWithPopup(auth, googleAuth)
+};
+
+export default loginWithGoogle;
