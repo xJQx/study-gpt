480d0ec309d72ec7e6e1505b6b06912dfb5a2f99
Jing Qiang <tohjingqiang@gmail.com>
2023-05-29 10:19:33 +0800

    feat: add ButtonLink component

---
+++ components/ButtonLink.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+interface ButtonLinkProps {
+  label: string;
+  href: string;
+}
+export const ButtonLink = ({ label, href }: ButtonLinkProps) => {
+  return (
+    <Link
+      href={href}
+      className="bg-brand-pink hover:bg-brand-red text-white px-[16px] py-[10px] mt-8 lg:mt-0 w-max block"
+    >
+      <div className="block">{label}</div>
+    </Link>
+  );
+};
