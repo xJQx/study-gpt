bc3fe9df07c36811129582e7977c7212437a018e
yantattan <84306837+yantattan@users.noreply.github.com>
2023-05-30 02:39:19 +0800

    feat: functionality integrated with api (#15)
    * feat: flash card functionality with dummy data
    
    * refactor: flashcard code organization structure
    
    * feat: input notes page and submission with dummy data
    
    * refactor: correct dummy data location to prepare for using actual data and flash cards back button
    
    * feat: restart flash cards test
    
    * chore: remove unused import
    
    * chore: reformatting to prettier expectation
    
    * chore: reformatting to prettier expectation
    
    * chore: format code
    
    * feat: functionalities work with api
    
    * chore: rm unused api modal
    
    ---------
    
    Co-authored-by: Jing Qiang <tohjingqiang@gmail.com>
    Co-authored-by: Setsuna Yukiii <siang-meng@live.com>

--- features/flashcard/components/FlashCardMainLayout.tsx
+++ features/flashcard/components/FlashCardMainLayout.tsx
@@ -7,24 +7,21 @@ export const FlashCardMainLayout = () => {
   const [notes, setNotes] = useState('');
   const [questions, setQuestions] = useState([{ question: '', answer: '' }]);
 
-  function submitNotes(notes: string) {
-    setNotes(notes);
-    // TODO: Send notes to api, receive response. If api did not return an error message, toggle to flash cards; otherwise
-    // notify error to user
-    setQuestions([
-      {
-        question:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam etiam erat velit scelerisque in dictum non?',
-        answer:
-          'Ac ut consequat semper viverra nam libero justo laoreet sit. Libero nunc consequat interdum varius sit amet mattis vulputate enim. Sapien et ligula ullamcorper malesuada proin libero nunc. Tincidunt dui ut ornare lectus sit.'
-      },
-      {
-        question:
-          'Suspendisse potenti nullam ac tortor vitae purus faucibus ornare?',
-        answer:
-          'Magna fringilla urna porttitor rhoncus dolor purus non enim praesent. Lectus nulla at volutpat diam ut venenatis. Urna id volutpat lacus laoreet.'
-      }
-    ]);
+  async function generateCards(notes: string) {
+    const res = await fetch('/api/notes', {
+			method: 'POST',
+			body: JSON.stringify({
+				userId: localStorage.getItem('userId'),
+				text: notes,
+				title: 'Generate questions',
+				apiKey: localStorage.getItem('apiKey')
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		const resJson = await res.json();
+    setQuestions(resJson.data.quiz);
     toggleNotesView(false);
   }
 
@@ -36,10 +33,10 @@ export const FlashCardMainLayout = () => {
   return (
     <div className="shadow p-10 bg-gray-100 mx-48 my-12 rounded-lg text-lg">
       {isNotesView ? (
-        <Notes submitNotes={submitNotes} />
+        <Notes submitNotes={generateCards} />
       ) : (
         <FlashCardLayout list={questions} newTest={newTest} />
       )}
     </div>
   );
-};
+}
